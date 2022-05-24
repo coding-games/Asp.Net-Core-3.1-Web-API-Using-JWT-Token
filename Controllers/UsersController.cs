4bc74f067aceb5d8c6b483cdbf4b0d352e7b6bae
gupta <gupta@LAPTOP-AGRSKCSA>
2022-05-24 18:53:06 +0530

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using API.Model;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+	[Authorize]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly IJWTManagerRepository _jWTManager;
+
+		public UsersController(IJWTManagerRepository jWTManager)
+		{
+			this._jWTManager = jWTManager;
+		}
+
+		[HttpGet]
+		public List<string> Get()
+		{
+			var users = new List<string>
+		{
+			"Lakshman Kumar Gupta",
+			"Tripti Gupta",
+		};
+
+			return users;
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		[Route("authenticate")]
+		public IActionResult Authenticate(Users usersdata)
+		{
+			var token = _jWTManager.Authenticate(usersdata);
+
+			if (token == null)
+			{
+				return Unauthorized();
+			}
+
+			return Ok(token);
+		}
+	}
+}
---
+++ Services/IJWTManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public interface IJWTManagerRepository
+    {
+        Tokens Authenticate(Users users);
+    }
+}
